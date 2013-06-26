0be1fabbf370461f42801fffb83434f99ad21ce9
Robin Hjelmeir <rhjelmeir@accusoft.com>
2013-06-26 16:33:20 -0400

    renaming package

--- app/code/community/Accusoft/Prizmclouddocsviewer/Block/Adminhtml/Prizmclouddocsviewer.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class Accusoft_Prizmclouddocsviewer_Block_Adminhtml_Prizmclouddocsviewer extends Mage_Adminhtml_Block_Template {
-
-
-
-}
--- app/code/community/Accusoft/Prizmclouddocsviewer/Helper/Data.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class Accusoft_Prizmclouddocsviewer_Helper_Data extends Mage_Core_Helper_Abstract
-{
-
-}
